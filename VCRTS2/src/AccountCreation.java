4d8cd15325335d556a7e8e60b5a8d4f0b64e2b8e
omar-narine20 <omar.narine20@my.stjohns.edu>
2022-10-04 09:05:34 -0400

    Added 2 text fields to account creation page. Working on framework for remainder of account creation page.

--- VCRTS2/src/AccountCreation.java
+++ VCRTS2/src/AccountCreation.java
@@ -8,18 +8,30 @@ public class AccountCreation {
 	private static final int FRAME_WIDTH = 500;
 	private static final int FRAME_HEIGHT = 500;
 
-
+	//Labels and variables for the Frame and Panel:
 	private JLabel test;
 	private JPanel panel;
+	private JTextField nameField;
+	private JTextField licensePlateField;
+	private JTextField IDNumberField; 
+	private JButton submitButton; 
+	
+	//Variables collecting and storing user information; to be written to file:
+	private boolean vehicleOwner; 
 	
 	public AccountCreation() {
 		test = new JLabel("Create Account Here"); 
 		test.setBounds(150,10,350,50);
+		test.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));	
 		
-		test.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+		nameField = new JTextField("Name: ");
+		nameField.setBounds(150, 110, 150, 25);
+		
+		IDNumberField = new JTextField("ID Number: ");
+		IDNumberField.setBounds(138, 160, 175, 25);
+		
 		
 		createPanel();
-		
 		
 		frame.setTitle("VCRTS"); //setting Title on top left corner of GUI
 		
@@ -28,7 +40,7 @@ public class AccountCreation {
 		
 		//INPUTTING SIZE OF GUI from VARIABLES
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setVisible(true);
 	}
@@ -39,8 +51,10 @@ public class AccountCreation {
 		panel.setLayout(null);
 		panel.setBackground(Color.CYAN);
 		panel.add(test);
+		panel.add(nameField);
+		panel.add(IDNumberField);
 
 		// adds to current object
-			frame.add(panel);
+		frame.add(panel);
 	}
 }
