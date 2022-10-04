cf0fba0bce2547596c521d77fda7e0355741ddc9
Rudy <rudyetzel@gmail.com>
2022-10-03 22:02:46 -0400

    Third commit,
    updated PostLogin class so that user can enter their name and be greeted
    with a welcome message.

--- VCRTS2/src/PostLogin.java
+++ VCRTS2/src/PostLogin.java
@@ -1,7 +1,10 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 //test
-public class PostLogin {
+public class PostLogin implements ActionListener{
 
     JFrame frame = new JFrame();
    
@@ -11,14 +14,25 @@ public class PostLogin {
 
 	private JLabel test;
 	private JPanel panel;
+	private JTextField nameField;
+	private JButton enterName;
+	private JLabel welcomeLabel;
+	private Color lav = new Color(221,160,221);
 	//test
+	
 	public PostLogin() {
 		test = new JLabel("Login Here. "); 
 		test.setBounds(200,10,350,50);
 		
 		test.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
 		
+		nameField = new JTextField("Name: ");
+		nameField.setBounds(175, 100, 150, 25);
+		
+		createButtonName();
+		createWelcomeLabel();
 		createPanel();
+		
 		
 		
 		frame.setTitle("VCRTS"); //setting Title on top left corner of GUI
@@ -33,13 +47,43 @@ public class PostLogin {
 	    frame.setVisible(true);
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		String name = nameField.getText();
+		welcomeLabel.setText("Welcome " + name);
+		
+	}
+	
+	public void checkName() {
+		
+	}
+	
+	public void createButtonName() {
+		enterName = new JButton("Enter");
+		enterName.setBounds(175, 125, 150, 25);
+		enterName.setBackground(lav);
+		enterName.addActionListener(this);
+	}
+	
+	public void createWelcomeLabel() {
+		welcomeLabel = new JLabel(" ");
+		welcomeLabel.setBounds(190, 200, 350, 50);
+		welcomeLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+	}
+	
 	private void createPanel() {
 		panel = new JPanel();
 		panel.setLayout(null);
 		panel.setBackground(Color.CYAN);
 		panel.add(test);
+		panel.add(nameField);
+		panel.add(enterName);
+		panel.add(welcomeLabel);
+		
 
 		// adds to current object
 		frame.add(panel);
 	}
+
+
+	
 }
