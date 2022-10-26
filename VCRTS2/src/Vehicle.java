40c30d6d51e444521c51a25ab2aecebc717d8ef1
tommydiakoo <thomas.diakogeorgios20@stjohns.edu>
2022-10-26 15:14:40 -0400

    Update Vehicle.java
    Vehicle Class additions

--- VCRTS2/src/Vehicle.java
+++ VCRTS2/src/Vehicle.java
@@ -1,8 +1,43 @@
-
 public class Vehicle {
 	
-	public Vehicle(String licensePlate, String carMake, String carModel, String carYear, String residencyTime) {
-		
+	private String carMake;
+	private String carModel;
+	private int carYear;
+	private String licensePlate;
+	private int residencyTime;
+	private boolean isIdle;
+	
+	public Vehicle(String carMake, String carModel, int carYear, String licensePlate, int residencyTime) {
+		this.carMake = carMake;
+		this.carModel = carModel;
+		this.carYear = carYear;
+		this.licensePlate = licensePlate;
+		this.residencyTime = residencyTime;
 	}
+
+	public String getCarMake() {
+		return carMake;
+	}
+
+	public String getCarModel() {
+		return carModel;
+	}
+
+	public int getCarYear() {
+		return carYear;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+	public int getResidencyTime() {
+		return residencyTime;
+	}
+
+	public boolean isIdle() {
+		return isIdle;
+	}
+	
 
 }
